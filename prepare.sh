fef8911f07ac7bcc6990f119a9b2937853f18935
Timor Kodal <timor@kodal.de>
2023-03-11 15:04:38 +0100

    add dependency management files

---
+++ prepare.sh
@@ -0,0 +1,10 @@
+
+python3 -m pip install --user pipenv
+python3 -m pip install --upgrade pip --user
+python3 -m pipenv install requests pyyaml
+
+# If you use a .venv - setup, adjust the above commands according to the location of your virtual environment:
+# e.g. /Users/teamore/Development/iovano/coyote/.venv/bin/python -m pip install requests pyyaml
+
+# Detailled instructions on how to set up python virtual environments for VS Code can be found here:
+# https://code.visualstudio.com/docs/python/environments#_creating-environments
