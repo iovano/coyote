bf21300d8a558de5a16b8d9de288899dbc96253e
Timor Kodal <timor@kodal.de>
2023-03-13 22:57:20 +0100

    add log level tags and redundancy reduction

--- classes/Logger.py
+++ classes/Logger.py
@@ -1,21 +1,34 @@
 import datetime
 
 class Logger():
-    verbosity = 2
+    verbosity = 4
     target = 'STD'
     mode = 'a+'                 
-    def __init__(self, target = 'STD', verbosity = 2):
+    levels = ['FATAL  ', 'CRITICAL', 'ERROR  ', 'WARN   ', 'NOTICE ', 'DEBUG  ']
+    lastMessage = None
+    lastMessageLevel = None
+    redundantMessages = 0
+    def __init__(self, target = 'STD', verbosity = 4):
         self.target = target
         self.verbosity = verbosity
 
-    def log(self, message, level = 2):
+    def log(self, message, level = 4):
         if self.verbosity >= level:
-            if self.target != 'STD':
-                logfile = open(self.target, self.mode)
-                logfile.write("["+self.getCurrentTimestamp()+"]: "+message+"\n")
-                logfile.close()
+            if message == self.lastMessage:
+                self.redundantMessages += 1
             else:
-                print("["+self.getCurrentTimestamp()+"]: "+message)
+                if (self.redundantMessages > 0):
+                    redundantMessage = self.lastMessage + " [repeated "+str(self.redundantMessages)+" times]"
+                    self.redundantMessages = 0
+                    self.log(redundantMessage, self.lastMessageLevel)
+                if self.target != 'STD':
+                    logfile = open(self.target, self.mode)
+                    logfile.write("["+self.getCurrentTimestamp()+"]: "+message+"\n")
+                    logfile.close()
+                else:
+                    print(self.getCurrentTimestamp()+" | "+(self.levels[level] if len(self.levels) > level else '')+" : "+message)
+                self.lastMessage = message
+                self.lastMessageLevel = level
 
-    def getCurrentTimestamp(self, format = "%Y-%m-%d %H:%M:%S"):
+    def getCurrentTimestamp(self, format = "%y-%m-%d %H:%M:%S"):
         return datetime.datetime.now().strftime(format)
