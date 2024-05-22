cb3bfb4bf76e98112cf9579fd7557487f43b962e
teamore <timor@kodal.de>
2024-05-22 20:32:20 +0200

    add descriptive comment

--- motionsensor.py
+++ motionsensor.py
@@ -10,7 +10,7 @@ def checkIfShairportIsNotStreaming(self, effectiveSensorState, sensorState):
         isStreaming = os.path.isfile("/tmp/shairport-playing")
         if (isStreaming):
             self.log("active shairport stream prevents Idle mode", 4)
-        return (not isStreaming)
+        return (not isStreaming) # if shairport is streaming, return False and hereby prevent state change
 
 motionSensor = MotionSensor(False)
 motionSensor.onBeforeTriggerStateChange = types.MethodType(checkIfShairportIsNotStreaming, motionSensor)
